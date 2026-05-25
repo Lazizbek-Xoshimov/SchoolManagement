b06c67917c1fb9b36efe89aac0028f584f669904
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-05-25 22:02:03 +0500

    Default Teacher model has been created

---
+++ Models/Teacher.cs
@@ -0,0 +1,9 @@
+namespace SchoolManagement.Models;
+
+public class Teacher
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Subject { get; set; }
+}
