d5d3faa5882a5a1c08e133dbb4f4e14681e2a98a
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-05-26 13:38:51 +0500

    PhoneNumber property replaced with Age

--- Models/Student.cs
+++ Models/Student.cs
@@ -4,5 +4,5 @@ public class Student
 {
     public int StudentId { get; set; }
     public string FullName { get; set; }
-    public string PhoneNumber { get; set; }
+    public int Age { get; set; }
 }
