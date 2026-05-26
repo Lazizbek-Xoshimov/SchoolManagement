2ed58b21a8bfe57fb330276656726d0c2db1bef5
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-05-26 05:16:02 +0500

    Create and Read for 10 students

--- Program.cs
+++ Program.cs
@@ -1,1 +1,40 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using SchoolManagement.Models;
+using SchoolManagement.Services.Students;
+
+namespace SchoolManagement;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        IStudentService studentService = new StudentService();
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Student student = new Student();
+
+            Console.WriteLine($"Enter student {i}'s details.");
+            Console.Write("Enter student's full name: ");
+            student.FullName = Console.ReadLine();
+
+            Console.Write("Enter student's phone number: ");
+            student.PhoneNumber = Console.ReadLine();
+
+            studentService.CreateStudent(student);
+        }
+
+        Student[] students = studentService.GetAllStudents();
+        foreach(Student item in students)
+        {
+            if (item is null)
+                continue;
+            Console.WriteLine(
+                $"""
+                StudentId: {item.StudentId}
+                Student full name: {item.FullName}
+                Student phone number: {item.PhoneNumber}
+
+                """);
+        }
+    }
+}
