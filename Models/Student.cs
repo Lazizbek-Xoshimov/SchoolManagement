2384c5a06c0729b806d7cc2732a4a75b8b9d5433
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-05-26 05:13:41 +0500

    Subjects removed from Student model

--- Models/Student.cs
+++ Models/Student.cs
@@ -2,8 +2,7 @@ namespace SchoolManagement.Models;
 
 public class Student
 {
-    public int Id { get; set; }
+    public int StudentId { get; set; }
     public string FullName { get; set; }
     public string PhoneNumber { get; set; }
-    public string[] Subjects { get; set; }
 }
