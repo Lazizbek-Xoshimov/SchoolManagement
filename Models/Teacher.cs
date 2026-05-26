8a68d8d7e6488bef9909c0a3eda25d2e6f79ed2f
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-05-26 19:14:21 +0500

    Teached model changed

--- Models/Teacher.cs
+++ Models/Teacher.cs
@@ -3,7 +3,6 @@ namespace SchoolManagement.Models;
 public class Teacher
 {
     public int TeacherId { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public string FullName { get; set; }
     public string Subject { get; set; }
 }
