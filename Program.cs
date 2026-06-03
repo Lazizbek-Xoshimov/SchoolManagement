023d6884af89bf9bce4b93e26da2f745c8fca503
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-06-03 18:21:17 +0500

    Calls were made without changes in program.cs

--- Program.cs
+++ Program.cs
@@ -6,64 +6,13 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.Clear();
-        Console.BackgroundColor = ConsoleColor.DarkBlue;
-        Console.ForegroundColor = ConsoleColor.Black;
-        Console.WriteLine("Welcome to SchoolManagement");
-        Console.ResetColor();
-        Console.WriteLine("1. Student Management");
-        Console.WriteLine("2. Teacher Management");
-        Console.WriteLine("0. Exit");
+        BaseMenu baseMenu = new BaseMenu();
+
+        baseMenu.ShowOptions();
+
         Console.Write("Select the necessary section: ");
         int option = int.Parse(Console.ReadLine());
 
-        switch (option)
-        {
-            case 1:
-                {
-                    StudentMenu studentMenu = new StudentMenu();
-                    string outputSelection = string.Empty;
-
-                    do
-                    {
-                        studentMenu.ShowOptions();
-                        Console.Write("Select the necessary section: ");
-                        int optionStudent = int.Parse(Console.ReadLine());
-
-                        studentMenu.SelectOption(optionStudent);
-
-                        Console.WriteLine("Do you want to exit the program?");
-                        Console.Write("(yes/no): ");
-                        outputSelection = Console.ReadLine();
-                    } while (outputSelection == "no");
-
-                    break;
-                }
-            case 2:
-                {
-                    TeacherMenu teacherMenu = new TeacherMenu();
-                    string outputSelection = string.Empty;
-
-                    do
-                    {
-                        teacherMenu.ShowOptions();
-                        Console.Write("Select the necessary section: ");
-                        int optionTeacher = int.Parse(Console.ReadLine());
-
-                        teacherMenu.SelectOption(optionTeacher);
-
-                        Console.WriteLine("Do you want to exit the program?");
-                        Console.Write("(yes/no): ");
-                        outputSelection = Console.ReadLine();
-                    } while (outputSelection == "no");
-
-                    break;   
-                }
-            case 0:
-                break;
-            default:
-                Console.WriteLine("You have selected the wrong section.");
-                break;
-        }
+        baseMenu.SelectOption(option);
     }
 }
