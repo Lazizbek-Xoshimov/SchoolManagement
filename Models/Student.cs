94cd2130dbce5e4bdaf51d1f2d2d1dea4c80758f
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-06-08 13:19:21 +0500

    Added Grade property

--- Models/Student.cs
+++ Models/Student.cs
@@ -5,6 +5,6 @@ public class Student
     public int StudentId { get; set; }
     public string FullName { get; set; }
     public int Age { get; set; }
-
+    public int Grade { get; set; }
     public int Course { get; set; }
 }
