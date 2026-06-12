9eeb99f2d51c810bbe430ad61d4ae525f20e6a91
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-06-12 19:14:52 +0500

    Saved to .json file for Generic Repository

--- Repositories/Generics/IRepository.cs
+++ Repositories/Generics/IRepository.cs
@@ -4,6 +4,7 @@ public interface IRepository<T>
 {
     public void Create(T value);
     public List<T> GetAll();
+    public T GetById(int id);
     public void Update(int id, T value);
     public void Delete(T value);
 }
--- Repositories/Generics/Repository.cs
+++ Repositories/Generics/Repository.cs
@@ -1,31 +1,56 @@
+using System.Text.Json;
+using SchoolManagement.Models;
+
 namespace SchoolManagement.Repositories.Generics;
 
-public class Repository<T> : IRepository<T> where T: class
+public class Repository<T> : IRepository<T> where T: Teacher
 {
-    private List<T> data;
+    private string path;
+    private string content;
+    private List<T> objects;
+    private int creationId;
 
     public Repository()
     {
-        this.data = new List<T>();
+        path = @"Data\teachers.json";
+        content = File.ReadAllText(path);
+        objects = JsonSerializer.Deserialize<List<T>>(content);
+
+        creationId = objects.Count.Equals(0) ? 0 : objects[objects.Count - 1].TeacherId + 1;
     }
 
     public void Create(T value)
     {
-        data.Add(value);
+        value.TeacherId = creationId ++;
+        objects.Add(value);
+
+        string data = JsonSerializer.Serialize(objects, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText(path, data);
     }
 
     public List<T> GetAll()
     {
-        return data;
+        return objects;
+    }
+
+    public T GetById(int id)
+    {
+        return objects[id];
     }
 
     public void Update(int id, T value)
     {
-        data[id] = value;
+        objects[id] = value;
+
+        string data = JsonSerializer.Serialize(objects, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText(path, data);
     }
 
     public void Delete(T value)
     {
-        data.Remove(value);
+        objects.Remove(value);
+
+        string data = JsonSerializer.Serialize(objects, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText(path, data);
     }
 }
